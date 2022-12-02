b6a3139825a4ad1a70a81199607b5ac53cae69d4
Henk Oordt <hd@oordt.dev>
2022-12-02 07:48:10 +0100

    Add installation instructions and slides for module 0 (#3)
    * Add installation instructions and slides for module 0
    
    * re-state learning objectives and link them to course modules
    
    * Alter learning objective to clarify the `why` of Rust
    
    * Update installation instructions to reflect clippy and rustfmt being installed by rustup
    
    * Add rust-toolchain.toml, make repo a workspace
    
    * Add repo URL and add link to instructions on cloning

---
+++ slides/layouts/three-slots.vue
@@ -0,0 +1,41 @@
+
+<!--
+  Usage:
+```md
+---
+layout: three-slots
+---
+# Center top
+This shows on the top
+::left::
+# Left
+This shows on the left
+::right::
+# Right
+This shows on the right
+```
+-->
+
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout ">
+    <div :class="props.class">
+      <slot />
+    </div>
+    <div class="two-columns w-full h-full grid grid-cols-2 gap-2">
+      <div class="col-left" :class="props.class">
+        <slot name="left"/>
+      </div>
+      <div class="col-right" :class="props.class">
+        <slot name="right" />
+    </div>
+    </div>
+  </div>
+</template>
